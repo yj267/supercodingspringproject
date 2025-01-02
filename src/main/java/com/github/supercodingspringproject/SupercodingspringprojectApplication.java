656d99dce4ba4acf52f55ec7611a8fee93dedc0a
yj267 <uujin1023@daum.net>
2025-01-02 17:28:15 +0900

    Initial commit

---
+++ src/main/java/com/github/supercodingspringproject/SupercodingspringprojectApplication.java
@@ -0,0 +1,13 @@
+package com.github.supercodingspringproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupercodingspringprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupercodingspringprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/supercodingspringproject/SupercodingspringprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.supercodingspringproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupercodingspringprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
